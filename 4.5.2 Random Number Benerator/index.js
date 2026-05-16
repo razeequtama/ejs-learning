d79e0853f8d482a2103939941b937a3eb77c9c1d
Raziq <rabbaniu74m4@gmail.com>
2026-05-16 13:47:33 +0700

    Starting project 4.5.2 Random Number Generator index.js

--- 4.5.2 Random Number Benerator/index.js
+++ 4.5.2 Random Number Benerator/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to structured server!' });
+  res.send("Welcome!");
 });
 
 // Error handler
