9ee39fb3842abf7576b3533ae9f5390870ebf25b
Raziq <rabbaniu74m4@gmail.com>
2026-05-16 11:50:52 +0700

    Finish project 4.5.1 Hello Dashboard

--- 4.5.1 Hello Dashboard/index.js
+++ 4.5.1 Hello Dashboard/index.js
@@ -1,1 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";    
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Set EJS as template engine
+app.set("view engine", "ejs");
+
+// Set views directory
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (r, s) => {
+    s.render("index", {username: "Raze", age: 20, hobby: "Story Telling"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
