390dc27e4fa0f3e7fbc2b7d4b0b569a3c22c9933
Raziq <rabbaniu74m4@gmail.com>
2026-05-26 23:50:04 +0700

    Set Frontend

--- 4.5.6 Guestbook App/server.js
+++ 4.5.6 Guestbook App/server.js
@@ -7,6 +7,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+let messages = [{
+    from: "Raze",
+    message: "Hey. Just wanna let you know that you're doing a good job the second you decided to be the better person."
+}];
+
 // Set EJS as template engine
 app.set("view engine", "ejs");
 
@@ -16,6 +21,14 @@ app.set("views", path.join(__dirname, "views"));
 // Set static default directory to public/
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (r, s) => {
+    s.render("index", {messages});
+})
+
+app.post("/", (r, s) => {
+    s.render("index", {messages});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
