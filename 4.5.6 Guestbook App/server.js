fde0afc4487c6294f26ef01aeb4c7c31f821394e
Raziq <rabbaniu74m4@gmail.com>
2026-05-27 13:59:45 +0700

    Complete 4-5-6

--- 4.5.6 Guestbook App/server.js
+++ 4.5.6 Guestbook App/server.js
@@ -8,9 +8,23 @@ const app = express();
 const PORT = 3000;
 
 let messages = [{
-    from: "Raze",
-    message: "Hey. Just wanna let you know that you're doing a good job the second you decided to be the better person."
-}];
+                    from: "Raze",
+                    message: "Hey. Just wanna let you know that you're doing a good job the second you decided to be the better person.",
+                    id: 1
+                },
+                {
+                    from: "Billy",
+                    message: "Scorched Earth.",
+                    id: 2
+                },
+                {
+                    from: "Frenchie",
+                    message: "I bet you never danced a day in your live.",
+                    id: 3
+                }
+];
+
+let id = 4;
 
 // Set EJS as template engine
 app.set("view engine", "ejs");
@@ -24,12 +38,34 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (r, s) => {
     s.render("index", {messages});
-})
+});
 
 app.post("/", (r, s) => {
-    s.render("index", {messages});
-})
+    const newMessage = {
+        from: r.body.nameInput,
+        message: r.body.messageInput,
+        id: id
+    };
+
+    id++;
+
+    messages.push(newMessage);
+
+    s.redirect("/");
+});
+
+app.get("/message", (r, s) => {
+    const messageId = Number(r.query.messageId);
+
+    const message = messages.find(m => m.id === messageId);
+
+    if (!message) {
+        return s.send("No message!");
+    }
+
+    s.send(message.message);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+});;
