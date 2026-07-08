93c0bc8b21551d07cb4fda39a4b765e4744237f4
Raziq <rabbaniu74m4@gmail.com>
2026-07-08 20:15:06 +0700

    finish 4-5-11

--- 4.5.11 Simple Quiz/server.js
+++ 4.5.11 Simple Quiz/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";    
+import { answers } from './answers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,6 +17,31 @@ app.set("views", path.join(__dirname, "views"));
 // Set static default directory to public/
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        correct: 0,
+        wrong: 0
+    });
+});
+
+app.post("/answer", (req, res) => {
+    let correct = 0;
+    let wrong = 0;
+
+    for (const key in answers) {
+        if (req.body[key] === answers[key]) {
+            correct++;
+        } else {
+            wrong++;
+        }
+    }
+
+    res.render("index", {
+        correct,
+        wrong
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
