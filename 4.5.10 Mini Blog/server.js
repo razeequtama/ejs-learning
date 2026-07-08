8e0492d532812c7b0a195d759ac2ec057cf76877
Raziq <rabbaniu74m4@gmail.com>
2026-07-08 14:22:03 +0700

    finish 4-5-10

--- 4.5.10 Mini Blog/server.js
+++ 4.5.10 Mini Blog/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";    
+import methodOverride from "method-override";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,19 +19,41 @@ app.set("views", path.join(__dirname, "views"));
 // Set static default directory to public/
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (r, s) => {
-    s.redirect("/users");
+    s.redirect("/blogs");
 })
 
-app.get("/users", (r, s) => {
+app.get("/blogs", (r, s) => {
     s.render("index", {blogs});
 })
 
-// app.get("/users/:id", (r, s) => {
-//     console.log(r.params.id);
-//     s.redirect("/users");
-// })
+app.get("/blogs/:id", (req, res) => {
+    const id = Number(req.params.id); // Convert URL parameter to a number
+
+    const blog = blogs.find(blog => blog.id === id);
+
+    if (!blog) {
+        return res.status(404).send("Blog not found");
+    }
+
+    res.render("blog", { blog });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = blogs.findIndex(blog => blog.id === id);
+
+    if (index === -1) {
+        return res.status(404).send("Blog not found");
+    }
+
+    blogs.splice(index, 1);
+
+    res.redirect("/blogs");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
